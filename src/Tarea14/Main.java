74418a61cfa0ce86c623e5fe209ae96d0600a475
LeonardoDDS <dan-130@hotmail.com>
2023-05-11 19:16:53 -0600

    Tarea 14

---
+++ src/Tarea14/Main.java
@@ -0,0 +1,9 @@
+package Tarea14;
+
+import Tarea13.VentanaSwing;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana ventana= new Ventana("ComboBox MVC");
+    }
+}
